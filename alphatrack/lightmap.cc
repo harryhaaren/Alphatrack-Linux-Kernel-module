3297736d6c365a290d2a722abe12bfcc019daf72
David Täht <d@teklibre.com>
2009-03-14 18:40:41 -0600

    Started doing it the C way. I "get" the C way.

---
+++ alphatrack/lightmap.cc
@@ -0,0 +1,11 @@
+signed char lightmap[8][6] = {
+{ LightAnySolo, -1, -1, -1, -1, -1 },
+{ LightWrite, -1, -1, -1, -1, -1 },
+{ LightRead,  -1, -1, -1, -1, -1 },
+{ LightTrackRec, LightPan, LightSend, LightEQ, LightPlugin, LightAuto},
+{ LightSolo, LightF1, LightF2, -1, LightF3, LightF4 },
+{ LightMute, LightTrackLeft, LightTrackRight, -1, LightPunch, LightFlip },
+{ LightShift, -1, -1, -1, -1, LightRec },
+{ -2, -2, -2, -2, -2, -2} 
+};
+
